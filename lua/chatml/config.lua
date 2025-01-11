48414a5b4421dad8d9fb23d7843e48d6fa80f0c6
S1M0N38 <bertolottosimone@gmail.com>
2025-01-11 17:45:45 +0100

    refactor: clean up config file
    At the moment, chatml.nvim does not define any config.
    However, I will keep this config if I need to add future configs

--- lua/chatml/config.lua
+++ lua/chatml/config.lua
@@ -1,14 +1,16 @@
----@class Config
+---@class ChatMLConfig
 local M = {}
 
----@class DefaultOptions
-M.defaults = { name = "John Doe" }
+-- NOTE: chatml.nvim does not define any options at the moment.
 
----@class Options
+---@class ChatMLOptions
+M.defaults = {}
+
+---@class ChatMLOptions
 M.options = {}
 
 ---Extend the defaults options table with the user options
----@param opts UserOptions: plugin options
+---@param opts ChatMLOptions: plugin options
 M.setup = function(opts)
   M.options = vim.tbl_deep_extend("force", {}, M.defaults, opts or {})
 end
